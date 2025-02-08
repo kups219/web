0df692bec5dce87f3de59694c7baf0e31cc77d9d
kups219 <cf514122@gmail.com>
2025-02-08 14:30:12 -0600

    Add files via upload

---
+++ paginainfo2025/index.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilo.css">
+    <title>info</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="#">Inicio</a>
+            <a href="#">Acerca de</a>
+            <a href="#">Portafolio</a>
+            <a href="#">Servicios</a>
+            <a href="#">Contacto</a>
+        </nav>
+        <section class="textos-header">
+        <h1>Quinto Informática 2025</h1>
+        <h2>Práctica de Taller</h2>
+        </section>
+       
+    </header>
+    <main>
+        <section class="contenedor sobre-nosotros">
+            <h2 class="titulo">Nuestro Producto</h2>
+            <div class="contenedor-sobre-nosotros">
+                <img src="img/images 1.jfif" alt="" class="imagen-about-us">
+                <div class="contenido-texto">
+                    <h3><span>1 </span>Los Mejores Productos</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia reiciendis temporibus quaerat corporis alias unde error itaque, neque voluptatem distinctio architecto dolore blanditiis! Sit, modi ipsa voluptas nostrum non maxime.</p>
+                    <h3><span>2 </span>Los Mejores Productos</h3>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit saepe dolores vero cum, itaque reiciendis non et aliquam dignissimos, magnam nisi voluptatum laboriosam. Totam vitae quae veritatis est cupiditate itaque.</p>
+                </div>
+            </div>
+        </section>
+        <section class="portafolio">
+            <div class="contenedor">
+                <h2 class="titulo">Que dice la chonta</h2>
+                <div class="galeria-portafolio">
+                    <div class="imagen-portafolio">
+                        <img src="img/images 2.jpg" alt="">
+                        <div class="hover-galeria">
+                            <img src="img/radio-waves_icon-icons.com_50054.png" alt="">
+                            <p>Nuestro Trabajo</p>
+                        </div>
+                    </div>
+
+                    <div class="imagen-portafolio">
+                        <img src="img/images 3..jfif" alt="">
+                        <div class="hover-galeria">
+                            <img src="img/radio-waves_icon-icons.com_50054.png" alt="">
+                            <p>Nuestro Trabajo</p>
+                        </div>
+                    </div>
+
+                    <div class="imagen-portafolio">
+                        <img src="img/images 4.jpg" alt="">
+                        <div class="hover-galeria">
+                            <img src="img/radio-waves_icon-icons.com_50054.png" alt="">
+                            <p>Nuestro Trabajo</p>
+                        </div>
+                    </div>
+
+                    <div class="imagen-portafolio">
+                        <img src="img/images 5.jpg" alt="">
+                        <div class="hover-galeria">
+                            <img src="img/radio-waves_icon-icons.com_50054.png" alt="">
+                            <p>Nuestro Trabajo</p>
+                        </div>
+                    </div>
+
+                    <div class="imagen-portafolio">
+                        <img src="img/images 6.jpg" alt="">
+                        <div class="hover-galeria">
+                            <img src="img/radio-waves_icon-icons.com_50054.png" alt="">
+                            <p>Nuestro Trabajo</p>
+                        </div>
+                    </div>
+
+                    <div class="imagen-portafolio">
+                        <img src="img/imagen 7.jfif" alt="">
+                        <div class="hover-galeria">
+                            <img src="img/radio-waves_icon-icons.com_50054.png" alt="">
+                            <p>Nuestro Trabajo</p>
+                        </div>
+                    </div>
+
+                    <div class="imagen-portafolio">
+                        <img src="img/images 8" alt="">
+                        <div class="hover-galeria">
+                            <img src="img/radio-waves_icon-icons.com_50054.png" alt="">
+                            <p>Nuestro Trabajo</p>
+                        </div>
+                    </div>
+
+                    <div class="imagen-portafolio">
+                        <img src="img/descarga 11.jfif" alt="">
+                        <div class="hover-galeria">
+                            <img src="img/radio-waves_icon-icons.com_50054.png" alt="">
+                            <p>Nuestro Trabajo</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="clientes contenedor">
+            <h2 class="titulo">Que dicen nuestros clientes</h2>
+         <div class="cards">
+            <div class="card">
+                <img src="img/Rostro  1.jpg" alt="">
+                <div class="contenido-texto-card">
+                    <h4>Nombre</h4>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt officiis inventore voluptatum tempora architecto voluptates nihil, a iste numquam soluta saepe minima ipsa ipsum autem consequatur impedit nesciunt deleniti facilis.</p>
+                </div>
+            </div>
+            <div class="card">
+                <img src="img/Rostro 2.jpg" alt="">
+                <div class="contenido-texto-card">
+                    <h4>Nombre</h4>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt officiis inventore voluptatum tempora architecto voluptates nihil, a iste numquam soluta saepe minima ipsa ipsum autem consequatur impedit nesciunt deleniti facilis.</p>
+                </div>
+            </div>
+        </div>
+        </section>
+        <section class="about-services">
+            <div class="contenedor">   
+                      <h2 class="titulo">Nuestros Servicios</h2>
+                      <div class="servicio-cont">
+                        <div class="servicio-ind">
+                            <img src="img/imagen 7.jfif" alt="">
+                            <h3>Proyecto</h3>
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores praesentium harum voluptatibus perferendis sunt recusandae, animi odio possimus et, asperiores facilis.</p>
+                        </div>
+
+                        <div class="servicio-ind">
+                            <img src="img/images 5.jpg" alt="">
+                            <h3>Proyecto</h3>
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores praesentium harum voluptatibus perferendis sunt recusandae, animi odio possimus et, asperiores facilis.</p>
+                        </div>
+
+                        <div class="servicio-ind">
+                            <img src="img/images 6.jpg" alt="">
+                            <h3>Proyecto</h3>
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores praesentium harum voluptatibus perferendis sunt recusandae, animi odio possimus et, asperiores facilis.</p>
+                        </div>
+                      </div>
+            </div>
+        </section>
+    </main>
+    <footer>
+        <div class="contenedor-footer">
+            <div class="content-foo">
+                <h4>Tel:</h4>
+                <p>0021-4578</p>
+            </div>
+
+            <div class="content-foo">
+                <h4>E-mail:</h4>
+                <p>WernonPeruano@cetach2.edu.gt</p>
+            </div>
+
+            <div class="content-foo">
+                <h4>Ubicación:</h4>
+                <p>Km.55.5 Carretera a el Salvador</p>
+            </div>
+        </div>
+        <h2 class="titulo-final">&copy;Fernando | CETACH2-2025 </h2>
+    </footer>
+</body>
+</html>
